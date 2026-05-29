12153cc6d6f8bc9c292e808d75dfeb0092667ec3
Eric Lehmer <elehmer@berkeley.edu>
2026-05-29 15:55:19 -0700

    Dealing with oddness of isactive being integer not boolean like elsewhere in db

--- database/scripts/sql/add_warmer_wetter_hydroclimate.sql
+++ database/scripts/sql/add_warmer_wetter_hydroclimate.sql
@@ -14,7 +14,7 @@ INSERT INTO hydroclimate (
     notes, hydroclimate_version_id
 ) VALUES
     ('CMIP6_EC-Earth3-Veg_SSP370', 'Warmer and Wetter', '1.2°C temperature increase and 4% precipitation increase over CV inflow basins',
-     TRUE, '2043', 1,
+     1, '2043', 1,
      'HF statistics are used to adjust Historical temperature, precipitation, and streamflow using a quantile-mapping approach.', 1.0);
 
 COMMIT;
