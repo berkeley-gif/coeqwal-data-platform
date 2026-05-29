47937582ea6ebee16b37009d2d93a990c46e5119
Eric Lehmer <elehmer@berkeley.edu>
2026-05-29 15:53:37 -0700

    Fix PSQL syntax errors

--- database/scripts/sql/add_warmer_wetter_hydroclimate.sql
+++ database/scripts/sql/add_warmer_wetter_hydroclimate.sql
@@ -15,6 +15,6 @@ INSERT INTO hydroclimate (
 ) VALUES
     ('CMIP6_EC-Earth3-Veg_SSP370', 'Warmer and Wetter', '1.2°C temperature increase and 4% precipitation increase over CV inflow basins',
      TRUE, '2043', 1,
-     'HF statistics are used to adjust Historical temperature, precipitation, and streamflow using a quantile-mapping approach.', 1.0)
+     'HF statistics are used to adjust Historical temperature, precipitation, and streamflow using a quantile-mapping approach.', 1.0);
 
 COMMIT;
