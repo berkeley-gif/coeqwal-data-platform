0bedf18551dbd0a4462a02e1ecf01cb1390ca6d5
Eric Lehmer <elehmer@berkeley.edu>
2026-06-11 14:54:21 -0700

    Changes from integer to numeric the tier value fields for continuous data

---
+++ database/scripts/sql/change_tier_result_field_types.sql
@@ -0,0 +1,14 @@
+-- change_tier_result_field_types.sql
+--
+-- Change integer fields to numeric for continuous tier values
+--
+-- Created by Eric Lehmer 6/11/2026
+BEGIN;
+
+ALTER TABLE tier_result ALTER COLUMN tier_1_value TYPE NUMERIC(5,3);
+ALTER TABLE tier_result ALTER COLUMN tier_2_value TYPE NUMERIC(5,3);
+ALTER TABLE tier_result ALTER COLUMN tier_3_value TYPE NUMERIC(5,3);
+ALTER TABLE tier_result ALTER COLUMN tier_4_value TYPE NUMERIC(5,3);
+ALTER TABLE tier_result ALTER COLUMN total_value TYPE NUMERIC(5,3);
+
+COMMIT;
