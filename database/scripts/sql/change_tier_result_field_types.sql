7ae392ede5c8ae522fd8a9608cd7706a8d35f5ee
Eric Lehmer <elehmer@berkeley.edu>
2026-06-11 15:06:01 -0700

    Allow for another digit

--- database/scripts/sql/change_tier_result_field_types.sql
+++ database/scripts/sql/change_tier_result_field_types.sql
@@ -5,10 +5,10 @@
 -- Created by Eric Lehmer 6/11/2026
 BEGIN;
 
-ALTER TABLE tier_result ALTER COLUMN tier_1_value TYPE NUMERIC(5,3);
-ALTER TABLE tier_result ALTER COLUMN tier_2_value TYPE NUMERIC(5,3);
-ALTER TABLE tier_result ALTER COLUMN tier_3_value TYPE NUMERIC(5,3);
-ALTER TABLE tier_result ALTER COLUMN tier_4_value TYPE NUMERIC(5,3);
-ALTER TABLE tier_result ALTER COLUMN total_value TYPE NUMERIC(5,3);
+ALTER TABLE tier_result ALTER COLUMN tier_1_value TYPE NUMERIC(6,3);
+ALTER TABLE tier_result ALTER COLUMN tier_2_value TYPE NUMERIC(6,3);
+ALTER TABLE tier_result ALTER COLUMN tier_3_value TYPE NUMERIC(6,3);
+ALTER TABLE tier_result ALTER COLUMN tier_4_value TYPE NUMERIC(6,3);
+ALTER TABLE tier_result ALTER COLUMN total_value TYPE NUMERIC(6,3);
 
 COMMIT;
