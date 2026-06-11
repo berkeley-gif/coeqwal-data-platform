3e4979d889ed43eab4dd83c030b4f9e4f01dd17d
Eric Lehmer <elehmer@berkeley.edu>
2026-06-11 14:46:44 -0700

    Add another decimal place - API can round to 2 for frontend.

--- database/scripts/sql/add_tier_continuous.sql
+++ database/scripts/sql/add_tier_continuous.sql
@@ -5,6 +5,6 @@
 -- Created by Eric Lehmer 6/9/2026
 BEGIN;
 
-ALTER TABLE tier_location_result ADD COLUMN IF NOT EXISTS tier_continuous NUMERIC(5,2);
+ALTER TABLE tier_location_result ADD COLUMN IF NOT EXISTS tier_continuous NUMERIC(5,3);
 
 COMMIT;
